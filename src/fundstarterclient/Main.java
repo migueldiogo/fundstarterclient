c5764ffda059c504e459960cafc5024655bc54c9
Sergio Pires <sppiires@gmail.com>
2015-10-09 22:54:39 +0100

    test

--- src/fundstarterclient/Main.java
+++ src/fundstarterclient/Main.java
@@ -3,7 +3,7 @@ package fundstarterclient;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Worlds!!");
+        System.out.println("Hello Worldgggg!!");
 
     }
 }
